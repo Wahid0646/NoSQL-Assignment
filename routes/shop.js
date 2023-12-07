c6d0317450b5838ce48b5a4a784d96a76f9bb9a2
Abdul Wahid <wahidshaik0646@gmaiil.com>
2023-12-07 15:14:54 +0530

    Getting order placed

--- routes/shop.js
+++ routes/shop.js
@@ -18,9 +18,9 @@ router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
-//router.get('/orders', shopController.getOrders);
+router.get("/orders", shopController.getOrders);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
-
+router.post("/create-order", shopController.postOrder);
 
 //router.get('/checkout', shopController.getCheckout);
 
