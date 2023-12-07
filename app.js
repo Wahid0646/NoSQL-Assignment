31d10034eba58c9fc2daa34a311471015e1fcb1b
Abdul Wahid <wahidshaik0646@gmaiil.com>
2023-12-07 15:37:03 +0530

    User Table

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -19,12 +19,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // app.use((req, res, next) => {});
 
-// app.use((req, res, next) => {
-//   User.findById("642811cdaa73d64dd9937a98").then((user) => {
-//     req.user = new User(user.name, user.email, user.cart, user._id);
-//     next();
-//   });
-// });
+app.use((req, res, next) => {
+  User.findById("643d788b766004f0e0226739").then((user) => {
+    req.user = user;
+    next();
+  });
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -32,9 +32,21 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://nidhijagga5239:ANni2616@cluster.rbln5pm.mongodb.net/shop?retryWrites=true&w=majority"
+    "mongodb+srv://wahidshaik0646:wahid%400646@cluster.rbln5pm.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "nidhi",
+          email: "nidhi@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
